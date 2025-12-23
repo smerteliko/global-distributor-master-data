c41932be2cf063ee5993e0e0e1e5dedda6e2b861
smerteliko <smerteliko@gmail.com>
2025-12-23 11:24:15 +0300

    initial commit

---
+++ src/main/java/com/gda/masterdata/MasterdataApplication.java
@@ -0,0 +1,13 @@
+package com.gda.masterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterdataApplication.class, args);
+	}
+
+}
