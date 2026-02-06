f4c22c824b139ca642d36a16ff372e96484e36ef
smerteliko <smerteliko@gmail.com>
2026-02-06 17:41:43 +0300

    fix: cache imports

--- src/main/java/com/gda/masterdata/config/CacheConfig.java
+++ src/main/java/com/gda/masterdata/config/CacheConfig.java
@@ -1,17 +1,10 @@
 package com.gda.masterdata.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import java.time.Duration;
 
